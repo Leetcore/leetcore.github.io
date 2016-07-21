90f939b2ece6c301bb64434723e11bdb03e8eba0
Alex F <Leetcore@users.noreply.github.com>
2016-07-21 22:14:45 +0200

    Add files via upload

--- blockfriends/main.js
+++ blockfriends/main.js
@@ -1,4 +1,5 @@
 var hardcore = 10
+var gameover = false
 
 var Player = class {
     constructor(playerName, id) {
@@ -6,7 +7,7 @@ var Player = class {
         this.moving = false
         this.name = playerName
         this.idle = function() {
-            this.autoMoving = setTimeout(actionTimer, 1000)
+            this.autoMoving = setTimeout(actionTimer, 200)
         }
         this.stopidle = function() {
             clearTimeout(this.autoMoving)
@@ -14,10 +15,7 @@ var Player = class {
         }
         this.init = function () {
             this.idle()
-            $("#player" + id).on( "click", function () {
-                // console.log("click")                
-                movePlayer(id, "hoch", 20, 300)
-            })
+            clickable(1)
         }
         this.renderMe = renderPlayer(this.id)
     }
@@ -30,22 +28,38 @@ function init() {
     Player1.init()
 }
 
+function clickable (id) {
+        $("#player" + id).on( "click", function () {        
+            // console.log("click")
+            if (!gameover) {
+                movePlayer(id, "hoch", 10, 300)
+                $("#player" + id).off()
+                $("#player" + id).css("cursor", "progress");
+                $("#player" + id + " svg rect").css("fill", "#476b4b");
+                clickme = setTimeout(function() {clickable(id); $("#player" + id).css("cursor", "pointer")}, 250)
+            }
+            if (gameover) {
+                looser()
+            }
+        })
+}
+
 
 // bewegung tut gut!
 function movePlayer(id, richtung, schritte, dauer) {
-    if (Player1.moving != true) {
+    if (!gameover) {
         var cords = $("#player"+ id).offset()
 
-        Player1.moving = true 
+        Player1.moving = true
 
-        if (cords.left < window.innerWidth * 0.9 && richtung == "links") {
+        if (cords.left > 100 && richtung == "links") {
             $("#player"+ id).animate({
                 left: "+=" + schritte,
                 right: "-=" + schritte,
             }, dauer, function() {
                 Player1.moving = false
             });
-        } else if (cords.left > window.innerWidth * 0.1 && richtung == "rechts") {
+        } else if (cords.left < window.innerWidth - 100 && richtung == "rechts") {
             $("#player"+ id).animate({
                 left: "-=" + schritte,
                 right: "+=" + schritte,
@@ -61,54 +75,68 @@ function movePlayer(id, richtung, schritte, dauer) {
                 bottom: "-=" + schritte
             }, dauer, function() {
                 Player1.moving = false
-                if ($("#player"+ id).offset().top >= 170) {
-                    looser()
-                } else {
-                    $("#punkte").text(parseInt($("#punkte").text()) + 1)
+                    if ($("#player"+ id).offset().top >= 175) {
+                        looser()
+                    } else {
+                        $("#punkte").text(parseInt($("#punkte").text()) + 1)
+                    }
                 }
-            });
+            )
         }
 
-        if (richtung == "hoch") {            
-            playAudio("https://www.youtube.com/audiolibrary_download?vid=2d671c3f7880968e")
-            $("#player"+ id).animate({
-                bottom: "+=" + schritte
-            }, dauer, function() {     
-                Player1.moving = false           
-                movePlayer(id, "runter", schritte, 200)
-            });            
+        if (richtung == "hoch") {                        
+            if (!gameover) {
+                playAudio("https://www.youtube.com/audiolibrary_download?vid=f5b3c23c023f2f44")
+                $("#player"+ id).animate({
+                    bottom: "+=" + schritte
+                }, dauer, function() {     
+                    Player1.moving = false
+                    movePlayer(id, "runter", schritte, 300)
+                });            
+            }
         }
     }
 }
 
 // VERLORREEEEN!
 function looser() {
+    gameover = true
+    clickme.clearTimeout
+    actionTimerTimeout.clearTimeout
+    Player1.stopidle()
+
     playAudio("https://www.youtube.com/audiolibrary_download?vid=13321fdd50ae3ee6")
     $("#status").text("VERLOREN!")
-    Player1.stopidle()
     $("#player1").off()
+    $("#player1").css("cursor", "not-allowed")
+    $("#player1 svg rect").css("fill", "#a53232")
+    $("#player1").css("bottom", "0")
 }
  
 // random timer
 function actionTimer() {
-    var random = randomNumberGen(0, 3)
-    var schritte = randomNumberGen(0, 10 + hardcore)
-    var dauer = randomNumberGen(300, 300 + hardcore)
-    switch (random) {
-        case 0:
-            // make it harder over timer
-            hardcore++
-            break
-        case 1:
-            // zufällig gehen
-            movePlayer(1, "links", schritte, dauer)
-            break
-        case 2:
-            // zufällig gehen
-            movePlayer(1, "rechts", schritte, dauer)
-            break
+    if (!gameover) {
+        var random = randomNumberGen(0, 1)
+        var schritte = randomNumberGen(10 + hardcore, 20 + hardcore)
+        var dauer = randomNumberGen(600 - hardcore, 900 - hardcore)
+        switch (random) {
+            case 0:
+                if (hardcore <= 100) {
+                    hardcore = hardcore + 5
+                }
+                // zufällig gehen
+                movePlayer(1, "links", schritte, dauer)
+                break            
+            case 1:
+                // zufällig gehen
+                if (hardcore <= 100) {
+                    hardcore = hardcore + 5
+                }
+                movePlayer(1, "rechts", schritte, dauer)
+                break
+        }
+        actionTimerTimeout = setTimeout(actionTimer, randomNumberGen(600 - hardcore, 1200 - hardcore))
     }
-    actionTimerTimeout = setTimeout(actionTimer, randomNumberGen(1000, 1000 + hardcore))
 }
 
 function randomNumberGen(min, max) {
@@ -120,7 +148,7 @@ function renderPlayer(id) {
         // render player
         $("#main").append('<div id="player'+ id +'" style="left:30%">'+
             '<svg width="150" height="150" viewBox="0 0 150 150">'+
-                '<rect width="100" height="100" x="25" y="50" style="fill: #000;"/>'+
+                '<rect id="body" width="100" height="100" x="25" y="50" style="fill: #000;"/>'+
                 //'<circle id="augeL" class="augenP1" cx="60" cy="90" r="5" stroke-width="3" fill="white" />'+
                 //'<circle id="augeR" class="augenP1" cx="90" cy="90" r="5" stroke-width="3" fill="white" />'+
                 //'<ellipse cx="25" cy="75" rx="10" ry="20" class="ohren" style="fill: #000000;stroke-width:2;transform: rotate(-30deg);transform-origin: 50% 50%;"></ellipse>'+
