1a83e7c839240c29dfdb7b6f4e80a03aa473d1a0
Alex F <Leetcore@users.noreply.github.com>
2016-08-04 22:45:03 +0200

    Add files via upload

---
+++ zombiehoelle/main.js
@@ -0,0 +1,61 @@
+var welle = 1
+var verteidigung = 1
+var spieleranzahl = 1
+
+function startTimer(duration) {
+    var timer = 60 * 5, minutes, seconds
+    var mytimer = setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10)
+
+        minutes = minutes < 10 ? "0" + minutes : minutes
+        seconds = seconds < 10 ? "0" + seconds : seconds
+
+        $('#timer').text("-" + minutes + ":" + seconds)
+
+        if (--timer < 0) {
+            clearInterval(mytimer)
+            setTimeout(function() {
+                ZombieAttack(welle)
+                startTimer(60 * 5)
+            }, 5000)
+        }
+    }, 1000);
+}
+
+jQuery(function ($) {
+    var fiveMinutes = 60 * 5, display = $('#timer')
+    startTimer(fiveMinutes)
+});
+
+function ZombieAttack(welle) {
+    var angriffstärke = randomNumberGen(5 + welle, (10 + welle) * spieleranzahl)
+    if (angriffstärke >= verteidigung) {
+        ZombieKampf()
+    } else {
+        // schaden berechnen
+    }
+}
+
+function ZombieKampf() {
+    // checkspieleranzahl
+
+    if (randomNumberGen(0, spieleranzahl) == 0) {
+        gameover()
+    }
+}
+
+function gameover() {
+
+}
+
+function randomNumberGen(min, max) {
+    return Math.round(Math.random() * (max - min)) + min;
+}
+
+function message(nachricht) {
+    $("#messages").append("<p>" + nachricht + "<p/>")
+    $("#messages").animate({
+        scrollTop: $("#messages").offset().top
+    }, 1000);
+}
