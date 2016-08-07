8d34db555274cd20a4947ad4fa0e8f26418d9cff
Alex F <Leetcore@users.noreply.github.com>
2016-08-07 16:20:09 +0200

    Add files via upload

--- zombiehoelle/main.js
+++ zombiehoelle/main.js
@@ -2,23 +2,37 @@ var welle = 1
 var verteidigung = 1
 var spieleranzahl = 1
 var wasser = 3
+var angriffstärke = 2
+isGameover = false
 
 var inventar = [
-    {name: "Brett", label: "ein Brett", anzahl: 0},
-    {name: "Stein", label: "einen Stein", anzahl: 0},
-    {name: "Sand", label: "Sand", anzahl: 0}
+    {name: "Brett", plural: "Bretter", anzahl: 0},
+    {name: "Stock", plural: "Stöcke", anzahl: 0},
+    {name: "Stein", plural: "Steine", anzahl: 0},
+    {name: "Sand", plural: "Sand", anzahl: 0},
+    {name: "Seil", plural: "Seile", anzahl: 0},
+    {name: "Metall", plural: "Metalle", anzahl: 0},
+    {name: "Schaufel", plural: "Schaufeln", anzahl: 0},
+    {name: "Pistole", plural: "Pistolen", anzahl: 0},
+    {name: "Talisman", plural: "Talismane", anzahl: 0},
+    {name: "Glas", plural: "Gläser", anzahl: 0},
+    {name: "TNT", plural: "TNT", anzahl: 0}
 ]
 
 var techtree = [
-    {name: "Pflock", label: "einen Pflock", permanent: false, verteidigung: 1, baukosten: [{name: "Brett", anzahl: 1}]},
-    {name: "Steinmauer", label: "eine Steinmauer", permanent: false, verteidigung: 10, baukosten: [{name: "Stein", anzahl: 10}]},
-    {name: "Bretterkreuz", label: "ein Bretterkreuz", permanent: false, verteidigung: 10, baukosten: [{name: "Brett", anzahl: 3}]},
-    {name: "Brunnen", label: "einen Brunnen", permanent: true, baukosten: [{name: "Brett", anzahl: 20}, {name: "Stein", anzahl: 10}]},
-    {name: "Werkstatt", label: "die Werkstatt", permanent: true, baukosten: [{name: "Brett", anzahl: 5}, {name: "Stein", anzahl: 7}]}
-]
-
-var buildings = [
-    
+    {name: "Pflock", permanent: false, verteidigung: 1, baukosten: [{name: "Brett", anzahl: 1}]},
+    {name: "Hacke", permanent: false, verteidigung: 3, baukosten: [{name: "Stock", anzahl: 1}, {name: "Stein", anzahl: 1}]},
+    {name: "Graben", permanent: false, verteidigung: 18, baukosten: [{name: "Schaufel", anzahl: 3}, {name: "Brett", anzahl: 5}]},
+    {name: "Steinmauer", permanent: false, verteidigung: 10, baukosten: [{name: "Stein", anzahl: 5}]},
+    {name: "Talis Auge", permanent: false, verteidigung: 20, baukosten: [{name: "Stock", anzahl: 1}, {name: "Talisman", anzahl: 1}]},
+    {name: "Kreuz", permanent: false, verteidigung: 5, baukosten: [{name: "Brett", anzahl: 3}]},
+    {name: "Pistole", permanent: false, verteidigung: 15, baukosten: [{name: "Pistole", anzahl: 1}]},
+    {name: "Stacheldraht", permanent: false, verteidigung: 10, baukosten: [{name: "Metall", anzahl: 3}]},
+    {name: "Schaufel", permanent: false, verteidigung: 0, baukosten: [{name: "Brett", anzahl: 1}, {name: "Metall", anzahl: 1}]},
+    {name: "Steinschleuder", permanent: false, verteidigung: 10, baukosten: [{name: "Stein", anzahl: 3}, {name: "Seil", anzahl: 1}]},
+    {name: "Sprengfalle", permanent: false, verteidigung: 50, baukosten: [{name: "TNT", anzahl: 3}, {name: "Brett", anzahl: 5}]},
+    {name: "Brunnen", permanent: true, baukosten: [{name: "Brett", anzahl: 3}, {name: "Stein", anzahl: 5}, {name: "Seil", anzahl: 1}, {name: "Schaufel", anzahl: 1}]},
+    {name: "Werkstatt", permanent: true, baukosten: [{name: "Brett", anzahl: 5}, {name: "Stein", anzahl: 7}]}
 ]
 
 var map = []
@@ -30,7 +44,8 @@ for (var x = 0; x < 16; x++) {
 }
 
 function startTimer(duration) {
-    var timer = 60 * 2, minutes, seconds
+  if (isGameover == false) {
+    var timer = 60 * 1.5, minutes, seconds
     mytimer = setInterval(function () {
         minutes = parseInt(timer / 60, 10)
         seconds = parseInt(timer % 60, 10)
@@ -42,12 +57,13 @@ function startTimer(duration) {
 
         if (--timer < 0) {
             clearInterval(mytimer)
-            setTimeout(function() {
+            myTimeout = setTimeout(function() {
                 ZombieAttack()
-                startTimer(60 * 2)
+                startTimer(60 * 1)
             }, 1000)
         }
     }, 1000);
+  }
 }
 
 jQuery(function ($) {
@@ -57,15 +73,14 @@ jQuery(function ($) {
 
 function ZombieAttack() {
     playAudio("https://www.youtube.com/audiolibrary_download?vid=bfb60515d518694b")
-    var angriffstärke = randomNumberGen(5 + welle, (10 + welle) * spieleranzahl)
-    if (angriffstärke >= verteidigung) {
+    angriffstärke = angriffstärke + randomNumberGen(10 + welle * 2, (30 + welle * 2) * spieleranzahl)
+    if (angriffstärke > verteidigung) {
         $("#messages").empty()
-        message("Die Zombies greifen mit " + angriffstärke + " Stärke an und du hast nur " + verteidigung + ". Jetzt entscheidet der Nahkampf: Überlebender gegen Zombie!")
-        verteidigung = 1
-        if (randomNumberGen(0, welle) == 1) {
+        message("Die Zombies greifen mit " + angriffstärke + " Stärke an und du hast nur " + verteidigung + " Verteidigung. Jetzt entscheidet der Nahkampf: Überlebender gegen Zombie!")
+        if (randomNumberGen(0, welle) !== 1) {
             gameover()
         } else {
-            message("Du hast den Angriff mit Glück überlebt.")
+            message("Du hast den Angriff mit viel Glück überlebt.")
             welle = welle + 1
             renderWelle()
         }
@@ -73,18 +88,31 @@ function ZombieAttack() {
         welle = welle + 1
         renderWelle()
         $("#messages").empty()
-        verteidigung = verteidigung - Math.round(angriffstärke / randomNumberGen(2, 5))
+        verteidigung = verteidigung - Math.round(verteidigung * 0.20) - Math.round(angriffstärke * 0.20)
+        if (verteidigung < 1) {
+            verteidigung = 10
+        }
         message("Der Zombieangriff wurde abgewehrt, aber deine Verteidigung hat Schaden erlitten. Du hast noch " + verteidigung + " Punkte.")
     }
+    randomItems()
+    wasser = wasser - 1
+    renderWater()
+    renderVerteidigung()
 }
 
-function gameover() {
+function gameover(type) {
+    isGameover = true
     playAudio("https://www.youtube.com/audiolibrary_download?vid=26fa4912b03fa459")
     clearInterval(mytimer)
     $("#frame").remove()
     $("body").addClass("gameover")
-    $("body").html("<div style='text-align: center; font-size:300%;'><b>Zombies fressen dein Gehirn.</b><br/>Du bist tot!</div>")
+    if (type == "verdurstet") {
+        $("body").html("<div style='text-align: center; font-size:300%;'><b>Du bist verdurstet.</b><br/>Du bist tot!</div><div style='text-align: center; margin-top: 20px'>Die Zombies fressen<br/>deine Leiche.</div>")
+    } else {
+        $("body").html("<div style='text-align: center; font-size:300%;'><b>Zombies fressen<br/> dein Gehirn.</b><br/>Du bist tot!</div><div style='text-align: center; margin-top: 20px'>Der Angriff mit Stärke " + angriffstärke + " konnte nicht<br/> von deiner Verteidigung aufgehalten werden.")
+    }
 }
+
 
 function randomNumberGen(min, max) {
     return Math.round(Math.random() * (max - min)) + min;
@@ -93,23 +121,26 @@ function randomNumberGen(min, max) {
 function message(nachricht) {
     $("#messages").append("<p>" + nachricht + "<p/>")
     $("#messages").animate({
-        scrollTop: 1000
+        scrollTop: $("#messages p").last().offset().top
     }, 500);
 }
 
 function checkbuilder() {
-    var canBuild = false
+    var canBuild = 0
     $("#builder").empty()
     for (var bb = 0; bb < techtree.length; bb++) {
+        canBuild = 0
         for (var kk = 0; kk < techtree[bb].baukosten.length; kk++) {
             for (var cc = 0; cc < inventar.length; cc++) {
-                if (techtree[bb].baukosten[kk].name == inventar[cc].name && inventar[cc].anzahl >= techtree[bb].baukosten[kk].anzahl) {
-                    canBuild = true
-                } else {
-                    canBuild = false
-                }
-                if (canBuild == true && cc == techtree[bb].baukosten.length - 1) {
-                    $("#builder").append("<div class='build' onclick='build(\""+ techtree[bb].name + "\")'>" + techtree[bb].name + " bauen</div>")    
+                if (techtree[bb].baukosten[kk].name == inventar[cc].name) {
+                    if (inventar[cc].anzahl >= techtree[bb].baukosten[kk].anzahl) {
+                        canBuild = canBuild + 1
+                    } else {
+                        canBuild = 0
+                    }
+                    if (canBuild == techtree[bb].baukosten.length) {
+                        $("#builder").append("<div class='build' onclick='build(\""+ techtree[bb].name + "\")'>" + techtree[bb].name + "</div>")    
+                    }
                 }
             }
         }
@@ -120,18 +151,22 @@ function build(building) {
     var bauKostenOK = false
     var tempVerteidigung = 0
     var tempName = ""
+    var tempInventar = ""
+    var onlyOnce = false
     for (var t = 0; t < techtree.length; t++) {
         if (building == techtree[t].name && techtree[t].permanent == false) {
             for (var kk = 0; kk < techtree[t].baukosten.length; kk++) {
                 for (var inv = 0; inv < inventar.length; inv++) {
-                    if (inventar[inv].name == techtree[t].baukosten[kk].name) {
-                        bauKostenOK = true
+                    if (inventar[inv].name == techtree[t].baukosten[kk].name && inventar[inv].anzahl >= techtree[t].baukosten[kk].anzahl) {
+                        inventar[inv].anzahl = inventar[inv].anzahl - techtree[t].baukosten[kk].anzahl
                         tempVerteidigung = techtree[t].verteidigung
                         tempName = techtree[t].name
-                        inventar[inv].anzahl = inventar[inv].anzahl - techtree[t].baukosten[kk].anzahl
+                        tempInventar = techtree[t].name
+                        bauKostenOK = true
                     }
                 }
             }
+
         } else if (building == techtree[t].name && techtree[t].permanent == true) {
           if (map[x][y] == 0) {
               for (var kk = 0; kk < techtree[t].baukosten.length; kk++) {
@@ -145,8 +180,16 @@ function build(building) {
                   }
               }
           } else {
-              message("Auf diesem Platz steht bereits etwas.")
+              message(map[x][y] + " ist im Weg.")
           }
+        }
+        if (building == techtree[t].name && techtree[t].permanent == false && bauKostenOK == true && onlyOnce == false) {
+            for (var inv = 0; inv < inventar.length; inv++) {
+                if (building == inventar[inv].name) {
+                    inventar[inv].anzahl = inventar[inv].anzahl + 1
+                }
+            }
+            onlyOnce == true
         }
     }
     if (bauKostenOK) {
@@ -154,6 +197,7 @@ function build(building) {
         message(tempName + " wurde gebaut.")     
     }
     checkbuilder()
+    renderPosition()
     renderVerteidigung()
 }
 
@@ -164,6 +208,7 @@ function move(direction) {
                 y = y + 1
                 wasser = wasser -1
                 dropStuff()
+                renderPosition()
             } else {
                   message("Hier geht es nicht weiter")
               }
@@ -173,6 +218,7 @@ function move(direction) {
                 y = y - 1
                 wasser = wasser -1
                 dropStuff()
+                renderPosition()
             } else {
                   message("Hier geht es nicht weiter")
               }
@@ -182,6 +228,7 @@ function move(direction) {
                 x = x + 1
                 wasser = wasser -1
                 dropStuff()
+                renderPosition()
             } else {
                  message("Hier geht es nicht weiter")
              }
@@ -191,49 +238,65 @@ function move(direction) {
                 x = x - 1
                 wasser = wasser -1
                 dropStuff()
+                renderPosition()
             } else {
                 message("Hier geht es nicht weiter")
             }
             break
     }
-    renderPosition()
     checkActions()
     renderWater()
 }
 
 function renderPosition() {
     if (map[x][y] == 0) {
-        $("#currentPosition").text("")
+        $("#currentPosition").text("Nichts")
     } else {
         $("#currentPosition").text(map[x][y])
     }
     if (map[x][y] == "Brunnen") {
-        wasser = wasser + randomNumberGen(3, 5)
+        wasser = wasser + randomNumberGen(15 + Math.round(welle / 2), 25 + welle)
         message("Du hast dir Wasser aus dem Brunnen geholt.")
     }
     if (map[x][y] == "Werkstatt") {
-        
+        if (randomNumberGen(0, 2) == 0) {
+            inventar[0].anzahl = inventar[0].anzahl + 1
+            inventar[2].anzahl = inventar[2].anzahl + 1
+            wasser = wasser - 2
+            message("Du hast ein Brett und einen Stein produziert, aber es hat dich zusätzlich zwei Wasser gekostet.")
+        }
     }
     $("#cords").text("X: " + x + " Y: " + y)
 }
 
 function startPosition() {
-    playAudioBackground("https://www.youtube.com/audiolibrary_download?vid=ebcc847570aeee65")
+    playAudioBackground("https://www.youtube.com/audiolibrary_download?vid=712a11cf5689b55c")
     x = randomNumberGen(0, 15)
     y = randomNumberGen(0, 15)
+    brunnenX = x
+    brunnenY = y
     map[x][y] = "Brunnen"
-    message("Du bist an einem Brunnen. In zwei Minuten greifen die Zombies an. Du musst Material suchen, um dich zu verteidigen! Baue Verteidigungskonstruktionen.")
+    message("Du bist an einem Brunnen. In einer Minuten greifen die Zombies an. Du musst Material suchen, um dich zu verteidigen! Baue Verteidigungskonstruktionen und durchsuche die Karte nach besonderen Gegenständen.")
 }
 
 function dropStuff() {
     if (map[x][y] == 0) {
-        var random = randomNumberGen(0, 1)
+        var random = randomNumberGen(0, 4)
         switch (random) {
             case 0:
-                map[x][y] = "Brett"
+                message("Hier ist nichts was dir helfen könnte.")
                 break
             case 1:
+                map[x][y] = "Brett"
+                break
+            case 2:
                 map[x][y] = "Stein"
+                break
+            case 3:
+                map[x][y] = "Stock"
+                break
+            case 4: 
+                map[x][y] = "Metall"
                 break
         }
     }
@@ -243,7 +306,7 @@ function checkActions() {
     $("#actions").html("")
     for (var inv = 0; inv < inventar.length; inv++) {
         if (map[x][y] == inventar[inv].name) {
-            $("#actions").html("<div class='pick' onclick='take(\""+ inventar[inv].name + "\")'>aufnehmen</div>")
+            $("#actions").html("<div class='pick' onclick='take(\""+ inventar[inv].name + "\")'>mitnehmen</div>")
         }
     }
 }
@@ -252,11 +315,16 @@ function take(item) {
     for (var inv = 0; inv < inventar.length; inv++) {
         if (map[x][y] == inventar[inv].name && item == inventar[inv].name) {
             inventar[inv].anzahl++
-            message(inventar[inv].anzahl +" "+ inventar[inv].name + " im Inventar.")
+            if (inventar[inv].anzahl > 1) {
+                message(inventar[inv].anzahl +" "+ inventar[inv].plural + " im Inventar.")
+            } else {
+                message(inventar[inv].anzahl +" "+ inventar[inv].name + " im Inventar.")
+            }
             $("#actions").empty()
             $("#currentPosition").empty()
             map[x][y] = 0
             checkbuilder()
+            renderPosition()
         }
     }
 }
@@ -279,17 +347,65 @@ function renderVerteidigung() {
 }
 
 function renderWater() {
-    if (wasser < 3) {
-        message("Achtung! Dein Wasser wird knapp.")
+    if (wasser < 10) {
+        playAudio("https://www.youtube.com/audiolibrary_download?vid=07db49a10a4145d2")
+        message("Achtung! Dein Wasser wird knapp. Der Brunnen ist bei X: "+ brunnenX +" Y: "+ brunnenY +".")
     }
     if (wasser <= 0) {
-        gameover()
+        gameover("verdurstet")
     }
     $("#wasser").text(wasser + " Wasser")
 }
+
+function randomItems() {
+    var checkX = randomNumberGen(0, 15)
+    var checkY = randomNumberGen(0, 15)
+    if (map[checkX][checkY] == 0) {
+        var random = randomNumberGen(0, 4)
+        switch (random) {
+            case 0:
+                map[checkX][checkY] = "Glas"
+                break
+            case 1:
+                map[checkX][checkY] = "Pistole"
+                break
+            case 2:
+                map[checkX][checkY] = "Talisman"
+                break
+            case 3:
+                map[checkX][checkY] = "Seil"
+                break
+            case 4:
+                map[checkX][checkY] = "TNT"
+                break
+        }
+    }
+}
+
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    switch (evt.keyCode) {
+        case 37:
+            move("left");
+            break;
+        case 39:
+            move("right");
+            break;
+        case 38:
+            move("top")
+            break
+        case 40:
+            move("down")
+            break
+        case 32:
+            $(".pick").click()
+            break
+    }
+};
 
 startPosition()
 renderPosition()
 renderVerteidigung()
 renderWater()
 renderWelle()
+randomItems()
