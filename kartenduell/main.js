56f121b700f5e1db3b0ce38aabff1f44ab52ec65
Alex F <fuchs.1337@gmail.com>
2016-09-17 10:19:41 +0000

    erste auswahl

--- kartenduell/main.js
+++ kartenduell/main.js
@@ -1,19 +1,20 @@
 /* global Peer, peer, $ */
 
-var deineKarten = [
-    {name: "Bogenschütze", leben: 11, angriff: 7, verteidigung: 10, instant: "Ork"},
-    {name: "Ork", leben: 15, angriff: 5, verteidigung: 12, instant: "Ritter"},
-    {name: "Ritter", leben: 14, angriff: 5, verteidigung: 15, instant: "Drache"},
-    {name: "Hexe", leben: 10, angriff: 6, verteidigung: 8, instant: "Henker"},
-    {name: "Magier", leben: 12, angriff: 6, verteidigung: 10, instant: "Hexe"},
-    {name: "Bauer", leben: 9, angriff: 3, verteidigung: 6, instant: "Hofnarr"},
-    {name: "Hofnarr", leben: 5, angriff: 2, verteidigung: 5, instant: ""},
-    {name: "Henker", leben: 14, angriff: 4, verteidigung: 12, instant: "Bauer"},
-    {name: "Gute Fee", leben: 15, angriff: 5, verteidigung: 13, instant: "Hexe"},
-    {name: "Drache", leben: 18, angriff: 5, verteidigung: 12, instant: "Magier"}
+var alleKarten = [
+    {name: "Bogenschütze", leben: 11, angriff: 7, verteidigung: 10, instant: "Ork", benutzt: false},
+    {name: "Ork", leben: 15, angriff: 5, verteidigung: 12, instant: "Ritter", benutzt: false},
+    {name: "Ritter", leben: 14, angriff: 5, verteidigung: 15, instant: "Drache", benutzt: false},
+    {name: "Hexe", leben: 10, angriff: 6, verteidigung: 8, instant: "Henker", benutzt: false},
+    {name: "Magier", leben: 12, angriff: 6, verteidigung: 10, instant: "Hexe", benutzt: false},
+    {name: "Bauer", leben: 9, angriff: 3, verteidigung: 6, instant: "Hofnarr", benutzt: false},
+    {name: "Hofnarr", leben: 5, angriff: 2, verteidigung: 5, instant: "", benutzt: false},
+    {name: "Henker", leben: 14, angriff: 4, verteidigung: 12, instant: "Bauer", benutzt: false},
+    {name: "Gute Fee", leben: 15, angriff: 5, verteidigung: 13, instant: "Hexe", benutzt: false},
+    {name: "Drache", leben: 18, angriff: 5, verteidigung: 12, instant: "Magier", benutzt: false}
 ]
 
 var gegnerKarten = []
+var deineKarten = alleKarten
 var Ichbindran = true
 var Runde = 0
 var myId = 1
@@ -24,12 +25,31 @@ var conn;
 function init() {
     // kopiere array für gegnerKarten
     gegnerKarten = deineKarten
+    message("Willkommen")
     
-    sucheGegner()
+    // karten austeilen
+    neueKarte(3)
+    
+    //aufEmpfang()
+    botMode()
+    
+    
 }
 
-function renderKarte(kartenname) {
-    $("")
+function renderKarte(divstring) {
+    var kartename = $(divstring).attr("data-name")
+    
+    $(divstring).append('<div id="angriff"></div><div id="verteidigung"></div><div id="leben"></div><div id="name"></div>')
+    
+    for (var index = 0; index < gegnerKarten.length; index++) {
+        if (kartename == gegnerKarten[index].name) {
+            $(divstring).css("background-image", "")
+            $(divstring).find("#angriff").text(gegnerKarten[index].angriff)
+            $(divstring).find("#verteidigung").text(gegnerKarten[index].verteidigung)
+            $(divstring).find("#leben").text(gegnerKarten[index].leben)
+            $(divstring).find("#name").text(gegnerKarten[index].name)
+        }
+    }
 }
 
 function aufEmpfang() {
@@ -37,6 +57,7 @@ function aufEmpfang() {
     
     peer.on('open', function(id) {
         message("Deine Verbindungsnummer ist " + id)
+        sucheGegner()
         // console.log('My peer ID is: ' + id);
     });
     
@@ -53,14 +74,21 @@ function aufEmpfang() {
 }
 
 function sucheGegner() {
+    if (myId == yourId) {
+        yourId++
+    }
     conn = peer.connect(yourId);
+    
+    message("Connection mit " + yourId)
+    
+    console.log(conn)
     
     conn.on('connection', function(conn) {
         conn.on('data', function(data) {
             console.log('Received', data);
         });
         conn.send('Hello!');
-        message("Verbindung mit " +conn)
+        message("Verbindung mit " + conn)
     });
     
     conn.on('error', function(err) { 
@@ -76,7 +104,28 @@ function sucheGegner() {
 }
 
 function botMode() {
-    
+    if (Ichbindran == false) {
+        var zufallszahl = randomNumberGen(0, gegnerKarten.length) - 1
+        var zufallsKarte = gegnerKarten[zufallszahl].name
+        $("#gegner").attr("data-name", gegnerKarten[randomNumberGen(0, gegnerKarten.length) - 1].name)
+        renderKarte("#gegner")
+        gegnerKarten.splice(zufallszahl, 1)
+    }
+    setTimeout(botMode, 3000)
+}
+
+function randomNumberGen(min, max) {
+    return Math.round(Math.random() * (max - min)) + min;
+}
+
+function neueKarte(anzahl) {
+    for (var x = 0; x < anzahl; x++) {
+        var zufallszahl = randomNumberGen(0, deineKarten.length - 1)
+        var zufallsKarte = deineKarten[zufallszahl].name
+        $("#hand").append('<div class="karte" data-name="' + zufallsKarte + '" data-owner="spieler"></div>')
+        renderKarte(".karte[data-name='"+ zufallsKarte +"']")
+        deineKarten.splice(zufallszahl, 1)
+    }
 }
 
 function duell() {
@@ -90,16 +139,17 @@ function runde () {
     // spieler kann wieder ziehen oder wartet auf zug
     // 20 sec timeout
 }
+
 function message (text) {
     $("div#message").append("<p>" + text + "</p>")
-    if ($("div#message p").length > 20) {
+    if ($("div#message p").length > 5) {
         $("div#message p").eq(0).remove()
     }
 }
 
-$("div.karte").on('click', function() {
+$("#frame").on('click', 'div.karte[data-owner=spieler]', function() {
     // karte  
+    message($(this).attr("data-name"))
 })
 
-aufEmpfang()
-sucheGegner()
+init()
