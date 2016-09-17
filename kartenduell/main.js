c96b098eb1b7d2e18dd6d4f3499fd64a56c94516
Alex F <Leetcore@users.noreply.github.com>
2016-09-17 18:22:04 +0200

    Add files via upload

--- kartenduell/main.js
+++ kartenduell/main.js
@@ -1,16 +1,17 @@
 /* global Peer, peer, $ */
 
 var alleKarten = [
-    {name: "Bogenschütze", leben: 11, angriff: 7, verteidigung: 10, instant: "Ork", benutzt: false},
-    {name: "Ork", leben: 15, angriff: 5, verteidigung: 12, instant: "Ritter", benutzt: false},
-    {name: "Ritter", leben: 14, angriff: 5, verteidigung: 15, instant: "Drache", benutzt: false},
-    {name: "Hexe", leben: 10, angriff: 6, verteidigung: 8, instant: "Henker", benutzt: false},
-    {name: "Magier", leben: 12, angriff: 6, verteidigung: 10, instant: "Hexe", benutzt: false},
-    {name: "Bauer", leben: 9, angriff: 3, verteidigung: 6, instant: "Hofnarr", benutzt: false},
-    {name: "Hofnarr", leben: 5, angriff: 2, verteidigung: 5, instant: "", benutzt: false},
-    {name: "Henker", leben: 14, angriff: 4, verteidigung: 12, instant: "Bauer", benutzt: false},
-    {name: "Gute Fee", leben: 15, angriff: 5, verteidigung: 13, instant: "Hexe", benutzt: false},
-    {name: "Drache", leben: 18, angriff: 5, verteidigung: 12, instant: "Magier", benutzt: false}
+    {name: "Bogenschütze", leben: 11, angriff: 4, verteidigung: 10, instant: "Ork"},
+    {name: "Ork", leben: 15, angriff: 5, verteidigung: 12, instant: "Ritter"},
+    {name: "Ritter", leben: 14, angriff: 5, verteidigung: 15, instant: "Drache"},
+    {name: "Hexe", leben: 10, angriff: 6, verteidigung: 8, instant: "Henker"},
+    {name: "Magier", leben: 12, angriff: 6, verteidigung: 10, instant: "Hexe"},
+    {name: "Bauer", leben: 9, angriff: 3, verteidigung: 6, instant: "Hofnarr"},
+    {name: "Hofnarr", leben: 5, angriff: 2, verteidigung: 5, instant: ""},
+    {name: "Henker", leben: 14, angriff: 4, verteidigung: 12, instant: "Bauer"},
+    {name: "Gute Fee", leben: 15, angriff: 5, verteidigung: 13, instant: "Hexe"},
+    {name: "Drache", leben: 18, angriff: 5, verteidigung: 12, instant: "Magier"},
+    {name: "Seeschlange", leben: 20, angriff: 4, verteidigung: 15, instant: "Gute Fee"}
 ]
 
 var gegnerKarten = alleKarten.slice(0)
@@ -18,9 +19,11 @@ var deineKarten = alleKarten.slice(0)
 var Ichbindran = true
 var Runde = 0
 var myId = 1
-var yourId = 1
 var peer;
 var conn;
+
+var deinLeben = 100
+var gegnerLeben = 100
 
 function init() {
     message("Willkommen")
@@ -28,15 +31,13 @@ function init() {
     // karten austeilen
     neueKarte(3)
     
-    //aufEmpfang()
-    botMode()
-    runde()
+    aufEmpfang()
 }
 
 function renderKarte(divstring) {
     var kartename = $(divstring).attr("data-name")
     
-    $(divstring).append('<div id="angriff"></div><div id="verteidigung"></div><div id="leben"></div><div id="name"></div>')
+    $(divstring).append('<div id="leben"></div><div id="verteidigung"></div><div id="angriff"></div><div id="name"></div>')
     
     for (var index = 0; index < alleKarten.length; index++) {
         if (kartename == alleKarten[index].name) {
@@ -59,56 +60,49 @@ function aufEmpfang() {
     });
     
     peer.on('error', function(err) {
-        peer.disconnect()
         message(err)
-        if (myId <= 20) {
-            myId++
-            aufEmpfang ()
-        } else {
+        // wenn id besetzt spiel gegen diese id        
+        if (err.toString().indexOf("is taken") !== -1) {
             message("Keine Verbindungsnummer ist frei. Versuche es später wieder...")
+            myId++
+            aufEmpfang()
         }
     });
 }
 
 function sucheGegner() {
-    if (myId == yourId) {
-        yourId++
+    conn = peer.connect(prompt("Deine ID ist " + myId + ". Gegner ID:"));
+    if (conn.peer < myId) {
+        Runde 
     }
-    conn = peer.connect(yourId);
-    
-    message("Connection mit " + yourId)
-    
-    console.log(conn)
-    
+    runde()
+}
+
+function warteaufZug() {
     conn.on('connection', function(conn) {
         conn.on('data', function(data) {
             console.log('Received', data);
+            message("Zug erhalten" + data)            
+            clearTimeout(botTimeout)
+            Ichbindran = true
         });
-        conn.send('Hello!');
-        message("Verbindung mit " + conn)
+        conn.on('error', function(err) { 
+            message("Verbindung nicht möglich.")
+        });
     });
-    
-    conn.on('error', function(err) { 
-        message("Verbindung nicht möglich.")
-        if (yourId >= 20) {
-            yourId++
-            sucheGegner()
-        } else {
-            message ("Es wurde kein menschlicher Gegner gefunden. Bot-Modus startet.")
-            botMode()
-        }
-    });
+
+    var botTimeout = setTimeout(function () {
+        var computerKarte = botMode()
+        $("#gegner").attr("data-name", computerKarte)
+        renderKarte("#gegner")
+        message("Computer hat " + computerKarte + " gespielt.")
+        duell()
+        Ichbindran = true
+    }, 10000)
 }
 
 function botMode() {
-    if (Ichbindran == false) {
-        var zufallszahl = randomNumberGen(0, gegnerKarten.length) - 1
-        var zufallsKarte = gegnerKarten[zufallszahl].name
-        $("#gegner").attr("data-name", gegnerKarten[randomNumberGen(0, gegnerKarten.length) - 1].name)
-        renderKarte("#gegner")
-        gegnerKarten.splice(zufallszahl, 1)
-    }
-    setTimeout(botMode, 3000)
+    return gegnerKarten[randomNumberGen(0, gegnerKarten.length) - 1].name
 }
 
 function randomNumberGen(min, max) {
@@ -129,27 +123,42 @@ function neueKarte(anzahl) {
     }
 }
 
-function duell(werfaengtan) {
+function duell() {
     var karteGegner = $("#gegner").attr("data-name")
     var karteSpieler = $("#feld").attr("data-name")
 
-    
     if (karteGegner != "" && karteSpieler != "") {
-        if (werfaengtan == 0) {
-            $()
+        if (myId < gegnerId) {
+            parseInt($("#gegner #verteidigung").text()) = parseInt($("#gegner #verteidigung").text()) - parseInt($("#feld #angriff").text())            
         }
+
         // kampf
         // wiederholen
         setTimeout(duell, 1000)
+    } else {        
+        runde() 
     }
 }
 
 function runde () {
     if (Ichbindran == true) {
+        if ($("#hand .karte").length <= 2) {
+           neueKarte(1)
+        }       
         Kartespielbar (true)
+        message("Du bist an der Reihe!")
     } else {
+        message("Dein Gegner ist an der Reihe!")
         Kartespielbar (false)
+        warteaufZug()        
     }
+
+
+    $("#gegnerLeben").text(gegnerLeben)
+    $("#deinLeben").text(deinLeben)
+    $("#gegnerId").text(conn.peer)
+    $("#deineId").text(myId)
+    Runde++
 }
 
 function message (text) {
@@ -162,15 +171,23 @@ function message (text) {
 function Kartespielbar (bool) {
     if (bool == true) {
         $("#frame").on('click', 'div.karte[data-owner=spieler]', function() {
-            // karte  
-            message($(this).attr("data-name"))
             $("#feld").attr("data-name", $(this).attr("data-name"))
+            conn.send($(this).attr("data-name"))
             renderKarte("#feld")
             $(this).remove()
-            neueKarte(1)
+            Kartespielbar (false)
+            message("Du hast "+ $(this).attr("data-name") +" gespielt.")
+            Ichbindran = false
+            runde()
         })
     } else {
         $("#frame").off('click', 'div.karte[data-owner=spieler]')
     }
 }
+
 init()
+
+window.onbeforeunload = function(event)
+{
+    peer.destroy()
+};
