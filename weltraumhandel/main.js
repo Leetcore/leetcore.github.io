6dd5f59cf4e72cae2a0272524bd385723df1004c
Alex F <Leetcore@users.noreply.github.com>
2016-09-24 08:28:58 +0200

    Update main.js

--- weltraumhandel/main.js
+++ weltraumhandel/main.js
@@ -39,7 +39,7 @@ function init() {
     randomEvents()
     verbrauch()
     setTimeout(polizei, 2000)
-    nachricht("Du bist ein Schmuggler und willst dir eine goldene Nase verdienen. Pass dabei auf die Kontrolldrohnen auf!")
+    nachricht("Du bist ein Schmuggler und willst dir eine goldene Nase verdienen. Pass dabei auf die Kontrolldrohnen auf, wenn du eine Ladung hast!")
 }
 
 function generierePlanet (anzahl) {
