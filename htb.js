1cf3abdb02d15a8ac69c7b8198aa7877a37213eb
Alex K <Leetcore@users.noreply.github.com>
2017-03-24 14:27:15 +0100

    Update htb.js

--- htb.js
+++ htb.js
@@ -4,11 +4,14 @@ function checkLeet() {
     if (now.getHours() == 13 && now.getMinutes() == 37 || debug == true) {
       loaded = true
       document.getElementById('hacktheplanet').insertAdjacentHTML('beforeend', '<img src="htb.gif"><br/><p>13:37 Uhr!</p>')
+      document.getElementById('hacktheplanet').style.display = "block"
     } else if (loaded == true) {
+      loaded = false
       document.getElementById('hacktheplanet').innerHTML = ""
+      document.getElementById('hacktheplanet').style.display = "none"
     }
 
-    setTimeout(checkLeet, 30000)
+    setTimeout(checkLeet, 5000)
   } catch (e) {
     console.log(e.message)
   }
